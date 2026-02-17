57b94219e5642979fe52cb6b5babb0f6c42a5889
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-17 14:08:48 +0000

    feat(types): create a type for the channel resource (model/struct)

--- crates/types/src/channel.rs
+++ crates/types/src/channel.rs
@@ -1,7 +1,9 @@
-#[derive(Clone, Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ChannelId(pub u64);
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Channel {
     pub id: ChannelId,
     pub name: String,
