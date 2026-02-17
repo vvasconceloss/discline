e2ce14a5077e5a588153a6a52362e8bc85a39def
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-17 23:52:12 +0000

    feat(cli): load the user configuration file

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,11 @@
-pub mod config;
+use anyhow::{Context, Result};
+use crate::config::load_config;
 
-fn main() {}
+pub mod config;
+pub mod errors;
+
+fn main() -> Result<()> {
+    let _config = load_config().context("Failed to initialize application configuration")?;
+
+    Ok(())
+}
