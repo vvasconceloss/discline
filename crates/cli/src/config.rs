687c75a6b576232a247a8495e8e8e914c482e99f
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-17 19:16:32 +0000

    feat(cli): create a function to load user settings
    User settings are stored in a .toml file, located in the config/discline path of the system

--- crates/cli/src/config.rs
+++ crates/cli/src/config.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::{fs, path::PathBuf};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct AuthConfig {
@@ -21,4 +22,20 @@ pub struct Config {
     pub auth: AuthConfig,
     pub ui: UiConfig,
     pub cache: CacheConfig,
+}
+
+pub fn load_config() -> Config {
+    let config_path = dirs::home_dir()
+        .unwrap_or_else(|| PathBuf::from("."))
+        .join(".config")
+        .join("discline")
+        .join("config.toml");
+
+    let content = fs::read_to_string(&config_path)
+        .unwrap_or_else(|_| panic!("Config not found at {:?}", config_path));
+
+    let config: Config =
+        toml::from_str(&content).expect("config.toml has invalid format or missing fields");
+
+    config
 }
