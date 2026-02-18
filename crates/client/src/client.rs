ccca16f68b40d71e6c3be832117adf7384937d9a
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-18 14:20:24 +0000

    feat(client): create a trait with the methods get messages, channel, and send message

--- crates/client/src/client.rs
+++ crates/client/src/client.rs
@@ -1,13 +1,20 @@
 use reqwest::Client;
+use types::{
+    channel::{Channel, ChannelId},
+    guild::GuildId,
+    message::Message,
+};
+
+use crate::errors::client::ClientError;
 
 #[derive(Debug)]
-pub struct RestClient {
+pub struct HttpClient {
     http: Client,
     token: String,
     base_url: String,
 }
 
-impl RestClient {
+impl HttpClient {
     const BASE_URL: &str = "https://discord.com/api/v10";
 
     pub fn new(token: String) -> Self {
@@ -29,4 +36,21 @@ impl RestClient {
     pub fn base_url(&self) -> &str {
         &self.base_url
     }
+}
+
+pub trait RestClient {
+    fn get_channels(
+        &self,
+        guild_id: GuildId,
+    ) -> impl Future<Output = Result<Vec<Channel>, ClientError>> + Send;
+    fn get_messages(
+        &self,
+        channel_id: ChannelId,
+        limit: u8,
+    ) -> impl Future<Output = Result<Vec<Message>, ClientError>> + Send;
+    fn send_message(
+        &self,
+        channel_id: ChannelId,
+        content: &str,
+    ) -> impl Future<Output = Result<Message, ClientError>> + Send;
 }
