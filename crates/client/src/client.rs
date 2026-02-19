aa2577ecf8988a053469379ed1a631f47a644cfc
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-19 10:27:35 +0000

    chore(client): remove the REST client trait to a separate file

--- crates/client/src/client.rs
+++ crates/client/src/client.rs
@@ -3,12 +3,6 @@ use reqwest::{
     header::{AUTHORIZATION, HeaderMap, HeaderValue, USER_AGENT},
 };
 use serde::de::DeserializeOwned;
-use std::future::Future;
-use types::{
-    channel::{Channel, ChannelId},
-    guild::GuildId,
-    message::Message,
-};
 
 use crate::errors::client::ClientError;
 
@@ -105,55 +99,6 @@ impl HttpClient {
     }
 }
 
-pub trait RestClient {
-    fn get_channels(
-        &self,
-        guild_id: GuildId,
-    ) -> impl Future<Output = Result<Vec<Channel>, ClientError>> + Send;
-    fn get_messages(
-        &self,
-        channel_id: ChannelId,
-        limit: u8,
-    ) -> impl Future<Output = Result<Vec<Message>, ClientError>> + Send;
-    fn send_message(
-        &self,
-        channel_id: ChannelId,
-        content: &str,
-    ) -> impl Future<Output = Result<Message, ClientError>> + Send;
-}
-
-impl RestClient for HttpClient {
-    async fn get_channels(&self, guild_id: GuildId) -> Result<Vec<Channel>, ClientError> {
-        let endpoint = format!("/guilds/{}/channels", guild_id);
-        self.request(Method::GET, &endpoint, None::<()>).await
-    }
-
-    async fn get_messages(
-        &self,
-        channel_id: ChannelId,
-        limit: u8,
-    ) -> Result<Vec<Message>, ClientError> {
-        let endpoint = format!("/channels/{}/messages?limit={}", channel_id, limit);
-        self.request(Method::GET, &endpoint, None::<()>).await
-    }
-
-    async fn send_message(
-        &self,
-        channel_id: ChannelId,
-        content: &str,
-    ) -> Result<Message, ClientError> {
-        let endpoint = format!("/channels/{}/messages", channel_id);
-
-        #[derive(serde::Serialize)]
-        struct SendMessageBody<'a> {
-            content: &'a str,
-        }
-
-        self.request(Method::POST, &endpoint, Some(SendMessageBody { content }))
-            .await
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -165,16 +110,5 @@ mod tests {
 
         assert_eq!(client.token(), &token);
         assert_eq!(client.base_url(), HttpClient::BASE_URL);
-    }
-
-    #[tokio::test]
-    async fn test_client_unauthorized() {
-        let client = HttpClient::new("invalid-token".to_string());
-        let result = client.get_channels(GuildId(123)).await;
-
-        match result {
-            Err(ClientError::Unauthorized) => (),
-            other => panic!("Expected Unauthorized error, got {:?}", other),
-        }
     }
 }
